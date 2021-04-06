e5768349258eabd67bcaf5d97d6123282ae93ca3
Christian <christian@sanglech.com>
2021-04-06 18:17:09 +0900

    Initial commit for bi directional support

---
+++ src/com/christian/hibernate/demo/GetInstructorDetailDemo.java
@@ -0,0 +1,57 @@
+package com.christian.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.christian.hibernate.demo.entity.Instructor;
+import com.christian.hibernate.demo.entity.InstructorDetail;
+import com.christian.hibernate.demo.entity.Student;
+
+public class GetInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		///create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		
+		//create session
+		
+		Session session= factory.getCurrentSession();
+		
+		try {
+			
+			//start txn
+			session.beginTransaction();
+			
+			//get instructor detail obj
+			int theId=1;
+			
+			// print instruct detail
+			
+			InstructorDetail tempInstructorDetail= session.get(InstructorDetail.class, theId);
+			
+			
+			System.out.println("tempInstructorDetail: "+ tempInstructorDetail);
+		
+			
+			// print associated instuctor
+			
+			System.out.println("The associated Instructor is: "+ tempInstructorDetail.getInstructor());
+			
+		
+			//comit txn
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+		}finally {
+			factory.close();
+		}
+	}
+
+}
