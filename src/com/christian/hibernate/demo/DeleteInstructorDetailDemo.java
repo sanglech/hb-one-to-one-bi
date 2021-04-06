175934ddb8072125a00c35b4dc9e9b0efc4a5379
Christian <christian@sanglech.com>
2021-04-06 18:27:49 +0900

    Cascade delete ALL for instructor detail

---
+++ src/com/christian/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,68 @@
+package com.christian.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.christian.hibernate.demo.entity.Instructor;
+import com.christian.hibernate.demo.entity.InstructorDetail;
+import com.christian.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+		
+		///create session factory
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		
+		//create session
+		
+		Session session= factory.getCurrentSession();
+		
+		try {
+			
+			//start txn
+			session.beginTransaction();
+			
+			//get instructor detail obj
+			int theId=2;
+			
+			// print instruct detail
+			
+			InstructorDetail tempInstructorDetail= session.get(InstructorDetail.class, theId);
+			
+			
+			System.out.println("tempInstructorDetail: "+ tempInstructorDetail);
+		
+			
+			// print associated instuctor
+			
+			System.out.println("The associated Instructor is: "+ tempInstructorDetail.getInstructor());
+			
+			//deleteing instructordetail with id 2
+			
+			session.delete(tempInstructorDetail);
+			
+		
+			//comit txn
+			session.getTransaction().commit();
+			
+			System.out.println("Complete");
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
